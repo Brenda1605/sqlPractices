315d614eb86476b7f32c172ad3178e90c8a4a3a1
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-04-19 08:37:20 -0500

    Update practice3.sql

--- practice3.sql
+++ practice3.sql
@@ -23,45 +23,19 @@ SELECT (7 + 8) * 9;	-- answer: 135
 SELECT 3 ^ 3 - 1;   -- answer: 26
 SELECT 3 ^ (3 - 1); -- answer: 9
 
--- 2. MAKE OPERATIONS TO SUBSTRACT 2 COLUMNS
-SELECT county_name AS county,
-       state_name AS state,
-       births_2019 AS births,
-       deaths_2019 AS deaths,
-       births_2019 - deaths_2019 AS natural_increase
-FROM us_counties_pop_est_2019
-ORDER BY state_name, county_name;
-
--- 3. CALCULATE PERCENT CHANGE
-CREATE TABLE percent_change (
-    department text,
-    spend_2019 numeric(10,2),
-    spend_2022 numeric(10,2)
-);
-
-INSERT INTO percent_change
-VALUES
-    ('Assessor', 178556, 179500),
-    ('Building', 250000, 289000),
-    ('Clerk', 451980, 650000),
-    ('Library', 87777, 90001),
-    ('Parks', 250000, 223000),
-    ('Water', 199000, 195000);
-    
--- Math to calculate percent change
-SELECT department,
-       spend_2019,
-       spend_2022,
-       round( (spend_2022 - spend_2019) /
-                    spend_2019 * 100, 1) AS pct_change
-FROM percent_change;
-
--- 4. SUM AND AVG FUNCTIONS
+-- 2. SUM AND AVG FUNCTIONS
 SELECT sum(pop_est_2019) AS county_sum,
        round(avg(pop_est_2019), 0) AS county_average
 FROM us_counties_pop_est_2019;
 
--- 5. PERCENTILE FUNCTION
+-- 3. CALCULATE PERCENTAGE
+SELECT county_name AS county,
+       state_name AS state,
+       area_water::numeric / (area_land + area_water) * 100 AS pct_water
+FROM us_counties_pop_est_2019
+ORDER BY pct_water DESC;
+
+-- 4. PERCENTILE FUNCTION
 CREATE TABLE percentile_test (
     numbers integer
 );
@@ -76,3 +50,4 @@ SELECT
     WITHIN GROUP (ORDER BY numbers)
 FROM percentile_test;
 
+--------------------------------------------------------------------------------------
